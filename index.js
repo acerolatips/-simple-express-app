631a1e585a362804a910ae925801d1a4a3d57519
Hama <hamashi@88665a2e9fc9.ant.amazon.com>
2023-07-07 11:15:14 +0900

    Add logger[Morgan - tiny -]

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-
 const express = require('express');
+const logger = require('morgan')
 const app = express();
 const port = 3000;
 
+app.use(logger('tiny'))
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
